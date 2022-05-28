2adf748d4a84e28e9e28daf9d8fb14891ce5c873
bathtaters <nomoremrnice7@gmail.com>
2022-05-28 00:42:17 -0400

    allow setting log level via env vars

--- src/config/log.adapter.js
+++ src/config/log.adapter.js
@@ -1,7 +1,25 @@
-module.exports = {
-  debug: console.debug,
-  info: console.info,
-  log: console.log,
-  warn: console.warn,
-  error: console.error,
+const logOrder = [
+  { name: 'error', func: console.error },
+  { name: 'warn',  func: console.warn  },
+  { name: 'log',   func: console.log   },
+  { name: 'info',  func: console.info  },
+  { name: 'debug', func: console.debug },
+]
+
+const logLevel = getLogLevel(process.env.LOG_LEVEL, getLogLevel('info'))
+
+module.exports = logOrder.reduce((logFuncs, { name, func }, idx) => {
+  logFuncs[name] = logLevel < idx ? () => {} : func
+  return logFuncs
+}, {})
+
+module.exports.info(`Log level set to ${logOrder[logLevel].name} (${logLevel} of ${logOrder.length - 1})`)
+
+
+// Get Log Level from number or log name
+function getLogLevel(logLevel, defaultLevel = 100) {
+  if (!isNaN(logLevel)) logLevel = +logLevel
+  else if (typeof logLevel === 'string') logLevel = logOrder.findIndex(({ name }) => name == logLevel.toLowerCase())
+  if (typeof logLevel !== 'number' || logLevel < 0) logLevel = defaultLevel
+  return logLevel
 }
