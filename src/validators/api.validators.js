27b7fd70ccc8dfd6c6d923ddf4f918ab3a2ca8f2
bathtaters <nomoremrnice7@gmail.com>
2022-05-27 23:39:24 -0400

    add CRUD and Swap validators

--- src/validators/api.validators.js
+++ src/validators/api.validators.js
@@ -1,5 +1,8 @@
 const validate = require('./shared.validators')
 
 module.exports = {
-  all: (model) => validate.byRoute(model)([], 'all', true),
+  all:    (model) => validate.byRoute(model)([    ], 'all', true),
+  idAll:  (model) => validate.byRoute(model)(['id'], 'all', true),
+  idOnly: (model) => validate.byRoute(model)(['id'], []),
+  swap:   (model) => validate.byRoute(model)([    ], { id: 'id', swap: 'id' }),
 }
