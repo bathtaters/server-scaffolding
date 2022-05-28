5664a5432a15a1b7a7ba2dafda6c8f5c7d00b90e
bathtaters <nomoremrnice7@gmail.com>
2022-05-27 23:38:13 -0400

    reorder buttons and remove blank Form fields

--- src/services/form.services.js
+++ src/services/form.services.js
@@ -1,4 +1,4 @@
-exports.labels = [ 'Add', 'Remove', 'Update', 'Reset' ]
+exports.labels = [ 'Add', 'Update', 'Remove', 'Reset' ]
 
 // Actions based on Form submit button label
 exports.modelActions = (Model) => ({
@@ -10,19 +10,25 @@ exports.modelActions = (Model) => ({
     if (!newId) throw new Error(`New entry was not created.`)
   },
 
-  // REMOVE
-  [exports.labels[1]]: ({ id }) => {
-    if (id !== 0 && !id) throw new Error(`Must specify ID to remove.`)
-    return Model.remove(id)
-  },
-
   // UPDATE
-  [exports.labels[2]]: ({ id, ...data }) => {
+  [exports.labels[1]]: ({ id, ...data }) => {
     if (id !== 0 && !id) throw new Error(`Must specify ID to update.`)
     if (!data || !Object.keys(data)) throw new Error(`Must include data to update.`)
     return Model.update(id, data)
   },
 
+  // REMOVE
+  [exports.labels[2]]: ({ id }) => {
+    if (id !== 0 && !id) throw new Error(`Must specify ID to remove.`)
+    return Model.remove(id)
+  },
+
   // RESET
   [exports.labels[3]]: () => Model.create(true)
 })
+
+
+// Filter function for Object
+exports.filterFormData = (formData, filterCb = (val,key) => val) => Object.entries(formData).reduce(
+  (filtered, [key, val]) => filterCb(val,key) ? Object.assign(filtered, { [key]: val }) : filtered
+, {})
