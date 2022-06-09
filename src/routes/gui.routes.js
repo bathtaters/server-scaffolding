b74593684bcb563e8e7fc31f7b5bde516dea3dd5
bathtaters <nomoremrnice7@gmail.com>
2022-06-09 03:57:55 -0400

    fix SWAP malfunction for GUI

--- src/routes/gui.routes.js
+++ src/routes/gui.routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router()
-const { urls } = require('../config/meta')
 const controllers = require('../controllers/gui.controllers')
+const swapControl = require('../controllers/api.controllers').swap
+const swapValidate = require('../validators/api.validators').swap
+const { urls } = require('../config/meta')
+
 const models = require('../models/all')
 
 Object.entries(models).forEach(([name, Model]) => {
   router.get( `${urls.base}${name}`,      controllers.modelDashboard(Model))
+  // GUI API methods (Use GUI creds instead of API token)
   router.post(`${urls.base}${name}/form`, controllers.form(Model))
+  router.post(`${urls.base}${name}/swap`, swapValidate(name), swapControl(Model))
 })
 
 router.get(urls.base, controllers.dashboardHome)
