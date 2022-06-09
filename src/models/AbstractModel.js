665ed54eaa628169213ec3b1fda7cacb0a7a7e9d
bathtaters <nomoremrnice7@gmail.com>
2022-06-09 03:06:12 -0400

    add Count entries & handle errors in Update/Remove

--- src/models/AbstractModel.js
+++ src/models/AbstractModel.js
@@ -16,18 +16,25 @@ class AbstractModel {
   }
 
   create(overwrite = false) {
-    return services.reset(getDb(), { [this.title]: this.schema }, overwrite)
+    return services.reset(getDb(), { [this.title]: this.schema }, overwrite).then(() => ({ success: true }))
   }
     
-  get(id = null) {
+  get(id = null, idKey = 'id') {
     if (!id) return services.all(getDb(), `SELECT * FROM ${this.title}`)
-    return services.get(getDb(), `SELECT * FROM ${this.title} WHERE id = ?`, [id])
+    return services.get(getDb(), `SELECT * FROM ${this.title} WHERE ${idKey} = ?`, [id])
+  }
+
+  count(id = null, idKey = 'id') {
+    return services.get(getDb(),
+      `SELECT COUNT(*) cnt FROM ${this.title}${id ? ` WHERE ${idKey} = ?` : ''}`,
+      id ? [id] : []
+    ).then((count) => count && count.cnt)
   }
     
   add(data) {
     data = services.sanitizeSchemaData(data, this.schema)
     const keys = Object.keys(data)
-    if (!keys.length) return Promise.reject('No valid data to add')
+    if (!keys.length) return Promise.reject(new Error('No valid data provided'))
   
     return services.get(getDb(), 
       `INSERT INTO ${this.title}(${ keys.join(', ') }) VALUES(${ keys.map(()=>'?').join(', ') }) RETURNING id`,
@@ -35,19 +42,31 @@ class AbstractModel {
     ).then((row) => row && row.id)
   }
     
-  update(id, data) {
+  update(id, data, idKey = 'id') {
+    if (!id) return Promise.reject(new Error('No ID provided'))
+
     data = services.sanitizeSchemaData(data, this.schema)
     const keys = Object.keys(data)
-    if (!keys.length) return Promise.reject('No valid data to update')
-  
-    return services.run(getDb(), 
-      `UPDATE ${this.title} SET ${ keys.map(k => k+' = ?').join(', ') } WHERE id = ?`,
-      [...Object.values(data), id]
-    )
+    if (!keys.length) return Promise.reject(new Error('No valid data provided'))
+
+    return this.count(id, idKey).then((exists) => {
+      if (!exists) throw new Error('ID not found: '+id)
+
+      return services.run(getDb(), 
+        `UPDATE ${this.title} SET ${ keys.map(k => k+' = ?').join(', ') } WHERE ${idKey} = ?`,
+        [...Object.values(data), id]
+      )
+    }).then(() => ({ success: true }))
   }
   
-  remove(id) {
-    return services.run(getDb(), `DELETE FROM ${this.title} WHERE id = ?`, [id])
+  remove(id, idKey = 'id') {
+    if (!id) return Promise.reject(new Error('No ID provided'))
+
+    return this.count(id, idKey).then((exists) => {
+      if (!exists) throw new Error('ID not found: '+id)
+      
+      return services.run(getDb(), `DELETE FROM ${this.title} WHERE ${idKey} = ?`, [id])
+    }).then(() => ({ success: true }))
   }
 }
 
