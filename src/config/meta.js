e6011a7996b9287bbe0adce16374ee2fee4a82e8
bathtaters <nomoremrnice7@gmail.com>
2022-06-09 03:05:01 -0400

    add reused URLs to Meta

--- src/config/meta.js
+++ src/config/meta.js
@@ -1,12 +1,23 @@
 const join = require('path').join
 const pkg = require('../../package.json')
+const cdnUrls = require('./constants/cdn.urls')
 const pkgCfg = pkg.config || {}
+const dbDir = process.env.DB_DIR ||  __dirname
+const protectedPrefix = 'admin'
 
 module.exports = {
   name: pkg.name || 'Untitled',
   version: pkg.version || '0',
-  protectedPrefix: 'admin',
   port: process.env.port || +pkgCfg.port || 8080,
   rootPath: join(__dirname,'..','..'), // Update if this file moves
-  dbPath: join(__dirname,'database.db'), // Update if this file moves
+  dbPath: join(dbDir, 'database.db'),
+  dbDir,
+  protectedPrefix,
+  urls: {
+    ...cdnUrls,
+    base: '/dashboard/',
+    logout: '/users/logout/',
+    users: '/users/',
+    login: '/login/',
+  },
 }
