0cc833f812ffd1714c4c1368a44d2b0397964576
bathtaters <nomoremrnice7@gmail.com>
2022-06-09 02:56:22 -0400

    add local backup for cdn files

--- src/config/constants/cdn.urls.js
+++ src/config/constants/cdn.urls.js
@@ -1,9 +1,11 @@
 module.exports = {
   jquery: {
+    // src: "/javascripts/jquery-3.6.0.min.js", // Local copy
     src: "https://code.jquery.com/jquery-3.6.0.min.js",
     integrity: "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=",
   },
   minicss: {
-    href: "https://gitcdn.link/repo/Chalarangelo/mini.css/master/dist/mini-dark.css",
+    // href: "/stylesheets/mini-dark.min.css", // Local copy
+    href: "https://cdnjs.cloudflare.com/ajax/libs/mini.css/2.3.7/mini-dark.css",
   },
 }
