f44fc88d4ec164e4f0bba546879ab7ed65608e04
bathtaters <nomoremrnice7@gmail.com>
2022-06-09 21:08:20 -0400

    add show/hide password buttons

---
+++ public/javascripts/togglePassword.js
@@ -0,0 +1,19 @@
+
+$(function() {
+  $('button.toggle-password').text('Show').attr('aria-label', 'Show password as plain text.');
+});
+
+$( 'button.toggle-password' ).click(function() {
+  var toggleButton = this
+  $(this).siblings('input').each(function() {
+    if ($(this).attr('type') === 'password') {
+      $(this).attr('type', 'text');
+      $(toggleButton).text('Hide');
+      $(toggleButton).attr('aria-label', 'Hide password.');
+    } else {
+      $(this).attr('type', 'password');
+      $(toggleButton).text('Show');
+      $(toggleButton).attr('aria-label', 'Show password as plain text.');
+    }
+  })
+});
