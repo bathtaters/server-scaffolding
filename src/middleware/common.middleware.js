3d2fa0a392ea486e7b8e5674350cc336284e9e2c
bathtaters <nomoremrnice7@gmail.com>
2022-06-14 06:16:29 -0400

    moved debug logging to log.middleware

--- src/middleware/common.middleware.js
+++ src/middleware/common.middleware.js
@@ -1,4 +1,3 @@
-const logger = require('../config/log.adapter')
 const { deepUnescape } = require('../utils/validate.utils')
 
 // Add deepUnescape to run on send args
@@ -8,25 +7,4 @@ const unescapeMiddleware = (_, res, next) => {
   next()
 }
 
-// If enabled, log all IO to logger.debug
-const enableDebugLog = typeof logger.logLevel === 'number' ? logger.logLevel > 3 : logger.logLevel === 'debug'
-
-const debugLog = (req, res, next) => {
-  if (enableDebugLog) {
-    // Log input
-    const { method, params, body, user } = req
-    const url = `${req.protocol}://${req.subdomains.concat('').join('.')}${req.hostname}${req.originalUrl}`
-    const cookies = [ { type: 'session', ...req.session.cookie }, ...(req.cookies || []), ...(req.signedCookies || []) ]
-    logger.debug('> REQUEST:', method, url, { params, body: { ...body }, cookies, user })
-
-    // Log output
-    res.sendIgnoreLog = res.send
-    res.send = (data) => {
-      logger.debug('> RESPONSE:', method, url, { status: res.statusCode, cookies: res.cookies, data })
-      return res.sendIgnoreLog(data)
-    }
-  }
-  next()
-}
-
-module.exports = [debugLog, unescapeMiddleware]
+module.exports = [unescapeMiddleware]
