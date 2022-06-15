c3e824f07f54b53c3c4db0266c04d815a5ab7a59
bathtaters <nomoremrnice7@gmail.com>
2022-06-14 21:35:48 -0400

    use profile URL from config

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,14 +2,17 @@ const router = require('express').Router()
 const controllers = require('../controllers/users.controllers')
 const validate = require('../validators/users.validators')
 
-router.get( '/',                             controllers.userTable)
-router.post('/form',         validate.form,  controllers.adminForm)
-router.post('/regenToken',   validate.regen, controllers.regenToken)
+const urls = require('../config/constants/urls.cfg')
+const profileUrl = urls.profile.replace(urls.users, '')
 
-router.get( '/profile',                      controllers.userProfile)
-router.post('/profile/form', validate.form,  controllers.userForm)
+router.get( '/',                                  controllers.userTable)
+router.post('/form',              validate.form,  controllers.adminForm)
+router.post('/regenToken',        validate.regen, controllers.regenToken)
 
-router.post('/login',        validate.login, controllers.login)
-router.get( '/logout',                       controllers.logout)
+router.get(`/${profileUrl}`,                      controllers.userProfile)
+router.post(`/${profileUrl}form`, validate.form,  controllers.userForm)
+
+router.post('/login',             validate.login, controllers.login)
+router.get( '/logout',                            controllers.logout)
 
 module.exports = router
