81d5645b3235ddfc4a4f74c62c36c4af5ead89b5
bathtaters <nomoremrnice7@gmail.com>
2022-06-28 01:18:56 -0400

    fix use PrimaryID as ID param

--- src/internal/controllers/api.controllers.js
+++ src/internal/controllers/api.controllers.js
@@ -2,32 +2,32 @@ const errors = require('../config/errors.internal')
 
 exports.create = (Model) => function (req,res,next) {
   if (!req.body || !Object.keys(req.body).length) return next(errors.noData())
-  return Model.add(req.body).then((id) => res.send({ id })).catch(next)
+  return Model.add(req.body).then((id) => res.send({ [Model.primaryId]: id })).catch(next)
 }
 
 exports.read = (Model) => function (req,res,next) {
-  return Model.get(req.params.id).then((data) =>
-    data ? res.send(data) : next(errors.noEntry(req.params.id || '[All]'))
+  return Model.get(req.params[Model.primaryId]).then((data) =>
+    data ? res.send(data) : next(errors.noEntry(req.params[Model.primaryId] || '[All]'))
   ).catch(next)
 }
 
 exports.update = (Model) => async function (req,res,next) {
-  if (req.params.id == null) return next(errors.noID())
+  if (req.params[Model.primaryId] == null) return next(errors.noID())
   if (!req.body || !Object.keys(req.body).length) return next(errors.noData())
   
-  const entry = await Model.get(req.params.id).catch(next)
-  if (!entry || !entry[Model.primaryId]) return next(errors.noEntry(req.params.id))
+  const entry = await Model.get(req.params[Model.primaryId]).catch(next)
+  if (!entry || !entry[Model.primaryId]) return next(errors.noEntry(req.params[Model.primaryId]))
 
   return Model.update(entry[Model.primaryId], req.body).then(() => res.send({ success: true })).catch(next)
 }
 
 exports.delete = (Model) => async function (req,res,next) {
-  if (req.params.id == null) return next(errors.noID())
+  if (req.params[Model.primaryId] == null) return next(errors.noID())
 
-  const entry = await Model.get(req.params.id).catch(next)
-  if (!entry || !entry[Model.primaryId]) return next(errors.noEntry(req.params.id))
+  const entry = await Model.get(req.params[Model.primaryId]).catch(next)
+  if (!entry || !entry[Model.primaryId]) return next(errors.noEntry(req.params[Model.primaryId]))
 
-  return Model.remove(req.params.id).then(() => res.send({ success: true })).catch(next)
+  return Model.remove(req.params[Model.primaryId]).then(() => res.send({ success: true })).catch(next)
 }
 
 const TEMP_ID = -11
