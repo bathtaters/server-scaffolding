494698bfe3c5b1e2d3b59648dfc06328e949d6a6
bathtaters <nomoremrnice7@gmail.com>
2022-06-28 04:52:34 -0400

    fix spacing

--- src/internal/routes/admin.routes.js
+++ src/internal/routes/admin.routes.js
@@ -8,16 +8,17 @@ const { admin, root } = require('../../config/urls.cfg').gui
 
 router.use(checkAuth(root.login, access.admin))
 
+// User Table
 router.get( admin.user,                       validate.page,      controllers.userTable.model)
 router.post(admin.user+admin.token,           validate.token,     actions.regenToken)
 router.get( admin.user+admin.find,            validate.find,      controllers.userTable.find)
 router.post(admin.user+admin.form,            validate.form,      actions.adminForm)
 router.post(admin.user+admin.form+admin.find, validate.formNoMin, actions.adminForm)
 
-// TO DO -- Add settings/logs
+// Logs/Settings
 router.get(admin.logs,              controllers.logList)
 router.get(admin.logs+'/:filename', controllers.logView)
 router.get(admin.home,              controllers.settings)
-router.post(admin.home+admin.form, validate.env, actions.settingsForm)
+router.post(admin.home+admin.form,  validate.env, actions.settingsForm)
 
 module.exports = router
