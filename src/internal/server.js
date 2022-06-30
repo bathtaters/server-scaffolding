5553b86de390735f22355606b9bcf1093a1c1f17
bathtaters <nomoremrnice7@gmail.com>
2022-06-29 20:19:14 -0400

    remove common.middleware (absorb into server.init)

--- src/internal/server.js
+++ src/internal/server.js
@@ -5,13 +5,12 @@ const urls = require('../config/urls.cfg')
 const express = require('express')
 const { join } = require('path')
 const startServer = require('./services/init.services')
-const { setup, middleware, routes } = require('../server.init')
+const customServer = require('../server.init')
 const { notRoute } = require('./utils/common.utils')
 // Middleware
 const exitMiddleware = require('express-graceful-exit').middleware
 const authMiddleware = require('./middleware/auth.middleware').initAuth()
 const logMiddleware  = require('./middleware/log.middleware')
-const commonMiddleware = require('../middleware/common.middleware')
 const errorMiddleware  = require('./middleware/error.middleware')
 // Routes
 const apiRoutes   = require('./routes/api.routes')
@@ -25,8 +24,7 @@ const server = express()
 server.set('trust proxy', 1)
 server.set('views', [join(rootPath, 'src', 'views'), join(rootPath, 'src', 'internal', 'views')])
 server.set('view engine', 'pug')
-
-if (setup) setup(server)
+customServer.setup && customServer.setup(server)
 
 // Server-level Middleware
 server.use(exitMiddleware(server))
@@ -35,19 +33,15 @@ server.use(express.urlencoded({ extended: false }))
 server.use(express.static(join(rootPath, 'public')))
 server.use('/', express.static(join(rootPath, 'public', 'root')))
 server.use(notRoute(urls.api.prefix), authMiddleware)
-commonMiddleware.all && server.use(commonMiddleware.all)
-commonMiddleware.api && server.use(urls.api.prefix, commonMiddleware.api)
-commonMiddleware.gui && server.use(notRoute(urls.api.prefix), commonMiddleware.gui)
-if (middleware) middleware(server)
+customServer.middleware && customServer.middleware(server)
 server.use(logMiddleware())
 
 // Routes
+customServer.routes && customServer.routes(server)
 server.use('/',                   rootRoutes)
 server.use(urls.api.prefix,       apiRoutes)
 server.use(urls.gui.basic.prefix, guiRoutes)
 server.use(urls.gui.admin.prefix, adminRoutes)
-
-if (routes) routes(server)
 
 // Error Handling
 server.use(urls.api.prefix, errorMiddleware.json)
