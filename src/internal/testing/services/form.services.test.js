bcaf51f312454683569aac68763ab1d457e652f9
bathtaters <nomoremrnice7@gmail.com>
2022-07-22 16:36:03 -0400

    fix name and add boolFields tests to filterForm

--- src/internal/testing/services/form.services.test.js
+++ src/internal/testing/services/form.services.test.js
@@ -23,6 +23,7 @@ describe('labelsByAccess', () => {
   })
 })
 
+jest.mock('../../utils/validate.utils', () => ({ parseBoolean: () => () => 'parsed' }))
 describe('filterFormData', () => {
   it('removes null values', () => {
     expect(filterFormData({ a: 1, b: null, c: 3 })).toEqual({ a: 1, c: 3 })
@@ -32,10 +33,17 @@ describe('filterFormData', () => {
     expect(filterFormData({ a: 1, b: '', c: 3 })).toEqual({ a: 1, c: 3 })
     expect(filterFormData({ a: 1, b: 2, c: '', d: '' })).toEqual({ a: 1, b: 2 })
   })
+  it('force includes boolFields', () => {
+    expect(filterFormData({ a: 1 }, ['b','c'])).toEqual({ a: 1, b: false, c: false })
+  })
+  it('runs boolFields through parseBool', () => {
+    expect(filterFormData({ a: 1, b: 0, c: true }, ['b','c','d']))
+      .toMatchObject({ a: 1, b: 'parsed', c: 'parsed', })
+  })
   it('allows custom filter', () => {
-    expect(filterFormData({ a: 1, b: 2, c: 3 }, (val) => val !== 2))
+    expect(filterFormData({ a: 1, b: 2, c: 3 }, [], (val) => val !== 2))
       .toEqual({ a: 1, c: 3 })
-    expect(filterFormData({ a: 1, b: 2, c: 3 }, (val,key) => key !== 'c'))
+    expect(filterFormData({ a: 1, b: 2, c: 3 }, [], (val,key) => key !== 'c'))
       .toEqual({ a: 1, b: 2 })
   })
 })
