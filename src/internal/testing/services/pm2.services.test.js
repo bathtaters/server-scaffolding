1e3ba8ab42f3c2ee67081ac8315327541734e100
bathtaters <nomoremrnice7@gmail.com>
2022-07-22 17:13:06 -0400

    update for double checking list before reset

--- src/internal/testing/services/pm2.services.test.js
+++ src/internal/testing/services/pm2.services.test.js
@@ -3,13 +3,16 @@ const { connect, disconnect, list, restart } = require('../../utils/pm2.promises
 const appName = require('../../../config/meta').name
 
 describe('pm2 services testing', () => {
+  const extraProc = {
+    name: appName, pm_id: 914, pm2_env: { NODE_APP_INSTANCE: "104" }, pid: 1500
+  }
   beforeEach(() => {
     process.env.NODE_APP_INSTANCE = "102"
-    list.mockResolvedValueOnce([
-      { name: appName, pm_id: 911, pm2_env: { NODE_APP_INSTANCE: "101" } },
-      { name: appName, pm_id: 912, pm2_env: { NODE_APP_INSTANCE: "102" } },
-      { name: appName, pm_id: 913, pm2_env: { NODE_APP_INSTANCE: "103" } },
-      { name: 'other', pm_id: 914, pm2_env: { NODE_APP_INSTANCE: "220" } },
+    list.mockResolvedValue([
+      { name: appName, pm_id: 911, pm2_env: { NODE_APP_INSTANCE: "101" }, pid: 1200 },
+      { name: appName, pm_id: 912, pm2_env: { NODE_APP_INSTANCE: "102" }, pid: 1300 },
+      { name: appName, pm_id: 913, pm2_env: { NODE_APP_INSTANCE: "103" }, pid: 1400 },
+      { name: 'other', pm_id: 915, pm2_env: { NODE_APP_INSTANCE: "220" }, pid: 4321 },
     ])
   })
 
@@ -21,14 +24,14 @@ describe('pm2 services testing', () => {
   it('restarts list items w/ appName', async () => {
     await restartCluster({})
     expect(restart).toBeCalledTimes(3)
-    expect(restart).toBeCalledWith(911, expect.anything())
-    expect(restart).toBeCalledWith(912, expect.anything())
-    expect(restart).toBeCalledWith(913, expect.anything())
+    expect(restart).toBeCalledWith(911)
+    expect(restart).toBeCalledWith(912)
+    expect(restart).toBeCalledWith(913)
   })
   it('restarts self last', async () => {
     await restartCluster({})
     expect(restart).toBeCalledTimes(3)
-    expect(restart).toHaveBeenNthCalledWith(3, 912, expect.anything())
+    expect(restart).toHaveBeenNthCalledWith(3, 912)
   })
   it('sets APP_INSTANCE for each', async () => {
     await restartCluster({})
@@ -43,6 +46,24 @@ describe('pm2 services testing', () => {
     await restartCluster({ testEnv: 100, otherEnv: 'test' })
     expect(process.env.testEnv).toBe('100')
     expect(process.env.otherEnv).toBe('test')
+  })
+  it('double checks proc exists before restarting', async () => {
+    list.mockReturnValueOnce(list().then((l) => l.concat(extraProc)))
+    await restartCluster({})
+    expect(restart).toBeCalledTimes(3)
+    expect(restart).toBeCalledWith(911)
+    expect(restart).toBeCalledWith(912)
+    expect(restart).toBeCalledWith(913)
+
+    // Confirm test premise is valid
+    restart.mockClear()
+    list.mockReturnValue(list().then((l) => l.concat(extraProc)))
+    await restartCluster({})
+    expect(restart).toBeCalledTimes(4)
+    expect(restart).toBeCalledWith(911)
+    expect(restart).toBeCalledWith(912)
+    expect(restart).toBeCalledWith(913)
+    expect(restart).toBeCalledWith(914)
   })
 })
 
