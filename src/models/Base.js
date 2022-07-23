0880211589e749e95d75145a7edbde0a7a371432
bathtaters <nomoremrnice7@gmail.com>
2022-07-23 01:44:36 -0400

    add explanation for copying/editing as template

--- src/models/Base.js
+++ src/models/Base.js
@@ -1,5 +1,8 @@
 const Model = require('../internal/models/Model')
 
+// new Model('base') // => Simple way to create a Model, add info to models.cfg
+
+// Advanced model, allows overriding defaults & adding custom operations
 class Base extends Model {
   constructor() { super('base') }
 }
--- src/testing/endpoint/base.api.test.js
+++ src/testing/endpoint/base.api.test.js
@@ -1,101 +1,60 @@
+// Copy this test as a starting point for testing new Model APIs
+
 const server = require('../../internal/server')
 const request = require('supertest')(server)
-
 const { getApiHeader } = require('../../internal/testing/endpoint.utils')
 
-const apiPrefix = '/api/base'
+const apiPrefix = '/api/base' // change 'base' to Model title
 
 describe('Test Base model API', () => {
   let header, testId
   beforeAll(() => getApiHeader().then((token) => header = token))
 
-  test('User token works', async () => {
+  test('Confirm login works', async () => {
     await request.get(apiPrefix).set(header).expect(200).expect('Content-Type', /json/)
   })
 
-  test('POST /', async () => {
+  test('Create entry (POST)', async () => {
     const res = await request.post(apiPrefix).set(header).expect(200).expect('Content-Type', /json/)
       .send({
+        // Set fields of new item
         data: "test"
       })
     expect(res.body).toEqual({ id: expect.any(Number) })
     testId = res.body.id
   })
 
-  test('POST / + id', async () => {
-    const res = await request.post(apiPrefix).set(header).expect(200).expect('Content-Type', /json/)
-      .send({
-        id: 12,
-        data: "idTest"
-      })
-    expect(res.body).toEqual({ id: 12 })
-  })
-
-  test('GET /', async () => {
-    const res = await request.get(apiPrefix).set(header).expect(200).expect('Content-Type', /json/)
-    expect(res.body).toHaveLength(2)
-    expect(res.body).toEqual(expect.arrayContaining([
-      {
-        id: testId,
-        data: "test"
-      },{
-        id: 12,
-        data: "idTest"
-      }
-    ]))
-  })
-
-  test('GET /[id]', async () => {
+  test('Retrieve entry (GET)', async () => {
     const res = await request.get(`${apiPrefix}/${testId}`).set(header).expect(200).expect('Content-Type', /json/)
     expect(res.body).toEqual({
+      // Expected fields (Including default values that will be auto set)
       id: testId,
       data: "test"
     })
   })
 
-  test('UPDATE /[id]', async () => {
-    const update = await request.put(`${apiPrefix}/12`).set(header).expect(200).expect('Content-Type', /json/)
+  test('Update entry (PUT)', async () => {
+    const update = await request.put(`${apiPrefix}/${testId}`).set(header).expect(200).expect('Content-Type', /json/)
       .send({
+        // Field(s) to update
         data: "new"
       })
     expect(update.body).toEqual({ success: true })
 
-    const res = await request.get(`${apiPrefix}/12`).set(header).expect(200).expect('Content-Type', /json/)
+    const res = await request.get(`${apiPrefix}/${testId}`).set(header).expect(200).expect('Content-Type', /json/)
     expect(res.body).toEqual({
-      id: 12,
+      // Confirm that only updated field(s) changed
+      id: testId,
       data: "new"
     })
   })
 
-  test('POST /swap', async () => {
-    const swap = await request.post(`${apiPrefix}/swap`).set(header).expect(200).expect('Content-Type', /json/)
-      .send({ id: testId, swap: 12 })
-    expect(swap.body).toEqual({ success: true })
-
-    const res = await request.get(apiPrefix).set(header).expect(200).expect('Content-Type', /json/)
-    expect(res.body).toHaveLength(2)
-    expect(res.body).toEqual(expect.arrayContaining([
-      {
-        id: 12,
-        data: "test"
-      },{
-        id: testId,
-        data: "new"
-      }
-    ]))
-  })
-
-  test('DELETE /[id]', async () => {
+  test('Delete (DELETE)', async () => {
     const del = await request.delete(`${apiPrefix}/${testId}`).set(header).expect(200).expect('Content-Type', /json/)
     expect(del.body).toEqual({ success: true })
 
     const res = await request.get(apiPrefix).set(header).expect(200).expect('Content-Type', /json/)
-    expect(res.body).toHaveLength(1)
-    expect(res.body).toEqual(expect.arrayContaining([
-      {
-        id: 12,
-        data: "test"
-      }
-    ]))
+    expect(res.body).toHaveLength(0)
+    expect(res.body).toEqual([])
   })
 })
