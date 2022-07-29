033f67dbdcf528d2a98359640c83299000426cdb
bathtaters <nomoremrnice7@gmail.com>
2022-07-29 03:32:20 -0400

    enable test model until release

--- src/models/_all.js
+++ src/models/_all.js
@@ -1,4 +1,4 @@
 module.exports = [
   require('./Base'),
-  // require('../internal/testing/Test.model'), // Uncomment to use Test model in App
+  require('../internal/testing/Test.model'), // Uncomment to use Test model in App
 ]
