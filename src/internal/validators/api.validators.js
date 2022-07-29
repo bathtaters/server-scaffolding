97b204ae8019a5afa2c77e4d973fb1ca19b01bfc
bathtaters <nomoremrnice7@gmail.com>
2022-07-28 21:58:03 -0400

    fix CREATE validation bug (swapped params/body)

--- src/internal/validators/api.validators.js
+++ src/internal/validators/api.validators.js
@@ -1,7 +1,7 @@
 const { byModel } = require('./shared.validators')
 
 module.exports = {
-  all:    (Model) => byModel(Model, [],    { params: 'all' }),
+  all:    (Model) => byModel(Model, 'all'),
   idAll:  (Model) => byModel(Model, 'all', { params: [Model.primaryId] }),
   idOnly: (Model) => byModel(Model, [],    { params: [Model.primaryId] }),
   swap:   (Model) => byModel(Model, { [Model.primaryId]: Model.primaryId, swap: Model.primaryId }, { optionalBody: false }),
