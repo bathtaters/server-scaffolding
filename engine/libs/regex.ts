44f141a07eec85d85bb1d1f9809a3d0501a24ec3
Nick C <bathtaters@gmail.com>
2023-03-23 14:42:05 -0400

    remove ts-ignore and improve escRegex function

--- engine/libs/regex.ts
+++ engine/libs/regex.ts
@@ -1,8 +1,15 @@
 import RE2 from 're2'
 
-// @ts-ignore
-const RegEx: typeof RE2 = (pattern, flags) => new RE2(pattern, flags)
-export default RegEx
+export default function RegEx(pattern: Pattern, flags?: string | Buffer) {
+    return typeof pattern === 'string' ? new RE2(pattern, flags) : new RE2(pattern)
+}
 
 const regexChars = new RE2(/([\.\^\$\(\[\]\)\|])/g)
-export const escapeRegexPattern = (string: string) => string.replace(regexChars,'\\$1') 
+export const escapeRegexPattern = (stringOrRegex: string | RE2 | globalThis.RegExp) =>
+    typeof stringOrRegex === 'string' ?
+        stringOrRegex.replace(regexChars,'\\$1') :
+        stringOrRegex.source
+
+
+export type RegExp = RE2
+export type Pattern = string | globalThis.RegExp | Buffer | RE2
