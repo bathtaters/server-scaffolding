a31592b173cf26e46a23dab6eef05af25d9661ba
Nick C <bathtaters@gmail.com>
2023-03-30 14:33:41 -0400

    mv isLogLevel from services & fix getMaxEntry TS

--- engine/utils/log.utils.ts
+++ engine/utils/log.utils.ts
@@ -1,11 +1,13 @@
-import type { LogLevels, LogObject } from '../types/log'
+import { logLevels, LogLevels, LogObject } from '../types/log.d'
 import { parseISO, formatLong, formatShort } from '../libs/date'
 
 
 /** Get highest value log from Levels array ({ levelName: levelValue, ... }) */
-export const getMaxEntry = (obj: Record<string,number>) =>
-  Object.entries(obj).reduce<{ key?: string, val?: number }>(
-    (max, [key, val]) => typeof max.val !== 'number' || val > max.val ? { key, val } : max,
+export const getMaxEntry = <Key extends string>(obj: Record<Key,number>) =>
+  Object.entries<number>(obj).reduce<{ key?: Key, val?: number }>(
+    (max, [key, val]) =>
+      typeof max.val !== 'number' || val > max.val ?
+        { key: key as Key, val } : max,
     {}
   )
 
@@ -15,6 +17,13 @@ export const getAllLevels = (formattedLog: (LogObject | null)[]) => formattedLog
   (levels, entry) => entry?.level && !levels.includes(entry.level) ? levels.concat(entry.level) : levels,
   []
 )
+
+
+/** Check that a string is a valid logLevel, if not throw an error */
+export const isLogLevel = (logLevel: string, levelType = ''): logLevel is LogLevels => {
+  if (logLevel in logLevels) return true
+  throw new Error(`${levelType}LogLevel "${logLevel}" is not valid. Must be one of: "${Object.values(logLevels).join('", "')}"`)
+}
 
 
 /** Format log file time */
