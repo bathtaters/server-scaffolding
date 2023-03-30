f319ceb34c81753ad51cf8a4e0eec27cebb9f3e2
Nick C <bathtaters@gmail.com>
2023-03-30 00:18:15 -0400

    copy JSDOC definitions from model.engine

--- engine/types/Model.d.ts
+++ engine/types/Model.d.ts
@@ -2,25 +2,90 @@ import type { HTMLType } from './gui.d'
 import type { ModelType, Limits} from './validate.d'
 import type { SQLType, ForeignKeyAction } from './db.d'
 
+/** Definitions for a column of the Model */
 export interface Definition {
-  typeStr:     `${ModelType | "string*"}${"" | "?" | "[]" | "[]?"}`,
-  limits?:      Limits,
-  default?:     any,
-  isPrimary?:   boolean,
-  getAdapter?:  Adapter,
-  setAdapter?:  Adapter,
-  html?:        false | string[] | number | HTMLType,
-  db?:          false | SQLType,
-  dbOnly?:      boolean,
 
+  /** Column type of form: type*[]? (Everything except 'type' is optional)
+  *   - type = string, uuid, b64[url], hex, date, datetime, boolean, int, float, object, any
+  *   - [] = array of type
+  *   - ? = column is optional
+  *   - \* = (Only allowed w/ type = string) allows symbols/spaces in string */
+  typeStr:     `${ModelType | "string*"}${"" | "?" | "[]" | "[]?"}`,
+
+  /** Object of column limits
+   *   - { min?, max? } | { array: { min?, max? }, elem: { min?, max? } }
+   *   - Sets limits on numbers, string length or array size */
+  limits?:      Limits,
+
+  /** Default value
+   *   - Default value to use for that column if nothing provided on creation 
+   *   - This value is run through setAdapter each time */
+  default?:     any,
+
+  /** if column is SQL primary key
+   *   - When no type/typeStr is provided,
+   *     it will be set as auto-incrementing Int */
+  isPrimary?:   boolean,
+  
+  /** Function called whenever this column is retrieved from the database
+   *   - PARAMS: (Column value from database, Entire row as an object)
+   *   - RETURN: Updated column value for user
+   *   - default: Converts data based on type */
+  getAdapter?:  Adapter,
+
+  /** Function called whenever this column is stored in the database (ie. add/update)
+   *   - PARAMS: (Column value from user, Entire row as an object)
+   *   - RETURN: Updated column value for database
+   *   - default: Converts data based on type */
+  setAdapter?:  Adapter,
+
+  /** Determines HTML generated for this column in GUI form
+   *   - string = <input> w/ this as 'type' attribute
+   *   - number = <textarea> w/ this as 'rows' attribute
+   *   - string[] = <option>/<select> tags with this as list options
+   *   - false = column is omitted from GUI form (For database-only types)
+   *   - default: auto-generated based on type */
+  html?:        false | string[] | number | HTMLType,
+
+  /** Type of schema for this column in database
+   *   - false = column is not in database (Only for UI validation)
+   *   - default: schema is auto-generated based on type */
+  db?:          false | SQLType,
+  
+  /** If column is internal to database only
+   *   - true = obscure column from non-raw get results 
+   *   - default: false */
+  dbOnly?:      boolean,
+  
+
+  /** Column base type (w/o *[]? suffixes)
+   *   - default: parsed from typeStr */
   type?:        ModelType,
+
+  /** If column can be empty ('?' suffix)
+   *   - default: parsed from typeStr */
   isOptional?:  boolean,
+
+  /** If column is an array of <type> ('[]' suffix)
+   *   - This will auto-create and link a related table for this column
+   *      unless "db" property is present
+   *   - default: parsed from typeStr */
   isArray?:     boolean,
+
+  /** If a string column will allow spaces & special characters ('*' suffix)
+   *   - default: parsed from typeStr */
   hasSpaces?:   boolean,
+
+  /** If this column is a BitMap (Binary data stored as an integer)
+   *   - default: false */
   isBitmap?:    boolean,
+
+  /** If the data in a string column represents HTML code
+   *   - default: false */
   isHTML?:      boolean,
 }
 
+/** Object to generate Foreign Key Reference SQL */
 interface ForeignKeyRef {
   /** Name of column in current table */
   key:    string,
@@ -44,7 +109,14 @@ export type SchemaBase = Record<string, any>
 
 export type Feedback = { success: boolean }
 export type CreateTableRefs = { [table: string]: ForeignKeyRef }
-export type Adapter = (value: any, data: object) => Promise<any> | any
+
+/**
+ * Callback that takes in a single piece of data and returns a modified version of that data
+ * @param value - original value
+ * @param data - full object that value comes from
+ * @returns adapted value
+ */
+export type Adapter = <In = Any, Out = any, D = Record<string, any>>(value: In, data: D) => Promise<Out> | Out
 
 export type ChangeCallback<Schema extends SchemaBase> =
   (update: Partial<Schema>, matching: Schema[]) => Promise<Schema | void> | Schema | void
