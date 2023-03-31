2595668eb7a3354745274f0474a53d3e3199f2f2
Nick C <bathtaters@gmail.com>
2023-03-31 17:38:33 -0400

    add RequireOne generic type, and add JSDOC

--- engine/types/global.d.ts
+++ engine/types/global.d.ts
@@ -1,24 +1,42 @@
 // -- TYPESCRIPT HELPERS -- \\
 
+/** Object of a single type value (Inverse of Record<>) */
 export type ObjectOf<T, K extends string | number | symbol = string | number | symbol> = { [K in P]: T }
 
+
+/** Object of nested objects <T>, OK = outermost key type, IK = inner object <T> key type */
 export type NestedObject<
     T extends ObjectOf<any, IK> = ObjectOf<any, IK>,
     OK extends string | number | symbol = string | number | symbol,
     IK extends string | number | symbol = string | number | symbol
 > = ObjectOf<T, OK>
 
+
+/** An object derived from a nested object using the outer objects' keys and selected props from the inner objects
+ *   - Example: O = { a: { i: 1 }, b: { i: 2 } }, K = "i", DerivedObject = { a: 1, b: 2 } */
 export type NestedObjectValue<O extends object, K extends keyof O[keyof O]> = Record<keyof O, O[keyof O][K]>
 
+
+/** A recursive array or object, recurs itself until End is not an array or object
+ *   - Emulates the return value from function called recursively on elements of an array/object */
 export type Recur<T, End = any> =
   T extends any[] ? Recur<T[number], End>[] :
   T extends object ? { [P in keyof T]: Recur<T[P], End> } :
     End
 
+
+/** An object that requires AT LEAST ONE of the Keys */
+export type RequireOne<T, Keys extends keyof T = keyof T> =
+    Pick<T, Exclude<keyof T, Keys>> & {
+        [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+    }[Keys]
+
+
+    
 // -- TYPESCRIPT FIXES -- \\
 
 export declare global {
-    // Fix Object return types
+    // TODO -- REMOVE THIS AND SEE WHAT IT FIXES && ADD TS-RESET NPM PKG
     interface ObjectConstructor {
       keys<T>(o: T): T extends object ? (keyof T)[] :
         T extends number ? [] :
