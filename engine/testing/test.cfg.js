e5518fd32eb6aeb0b125f1a6fe4b66700f50e7a6
Nick C <bathtaters@gmail.com>
2023-04-18 02:02:34 -0400

    convert to typescript

--- engine/testing/test.cfg.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Port to use for tests (0 will use random port for each instance)
-  port: 0,
-  // DB Source for tests (':memory:' will create temporary in-memory DB)
-  testDb: ':memory:',
-  // Force log level when testing & disable files, falsy value will use .env level
-  testLog: 'none',
-}
