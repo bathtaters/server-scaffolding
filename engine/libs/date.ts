d528ce6ea70e1b8d11784aa11a4682e716b444d1
Nick C <bathtaters@gmail.com>
2023-05-17 01:58:51 -0400

    add type guard to isDate

--- engine/libs/date.ts
+++ engine/libs/date.ts
@@ -1,12 +1,13 @@
-import { parseISO, differenceInMilliseconds, format, isDate, formatISO } from 'date-fns'
+import { parseISO, differenceInMilliseconds, format, isDate as isDateFNS, formatISO } from 'date-fns'
 
 const locale = require('date-fns/locale').enUS
 
-export { parseISO, isDate }
+export { parseISO }
 
 export const
   now = Date.now,
+  isDate = (value: any): value is Date => isDateFNS(value),
   msAgo = (date: number | Date) => differenceInMilliseconds(module.exports.now(), date),
-  formatLong =  (date?: number | Date) => date ? format(date, 'MM/dd/yy hh:mm:ssaaa', { locale }) : '-',
+  formatLong =  (date?: number | Date | null) => date ? format(date, 'MM/dd/yy hh:mm:ssaaa', { locale }) : '-',
   formatShort = (date?: number | Date) => date ? format(date, 'MM/dd HH:mm', { locale }) : '',
   formatDateISO = (date?: number | Date) => date ? formatISO(date, { representation: 'date' }) : ''
