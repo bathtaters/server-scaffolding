c259e40887c42de1b213967dd7e3f642b41c7d20
Nick C <bathtaters@gmail.com>
2023-05-21 13:25:27 -0400

    add dateOnly/timeOnly string formatters

--- engine/libs/date.ts
+++ engine/libs/date.ts
@@ -10,4 +10,8 @@ export const
   msAgo = (date: number | Date) => differenceInMilliseconds(module.exports.now(), date),
   formatLong =  (date?: number | Date | null) => date ? format(date, 'MM/dd/yy hh:mm:ssaaa', { locale }) : '-',
   formatShort = (date?: number | Date) => date ? format(date, 'MM/dd HH:mm', { locale }) : '',
+  formatDateLong =  (date?: number | Date | null) => date ? format(date, 'MM/dd/yy', { locale }) : '-',
+  formatDateShort = (date?: number | Date) => date ? format(date, 'MM/dd', { locale }) : '',
+  formatTimeLong =  (date?: number | Date | null) => date ? format(date, 'hh:mm:ssaaa', { locale }) : '-',
+  formatTimeShort = (date?: number | Date) => date ? format(date, 'HH:mm', { locale }) : '',
   formatDateISO = (date?: number | Date) => date ? formatISO(date, { representation: 'date' }) : ''
