e59e72dc4c47e8510eac24fb9ac9fb3760ca2b26
Nick C <bathtaters@gmail.com>
2023-06-03 19:35:28 -0400

    remove ts-ignores

--- src/models/_all.ts
+++ src/models/_all.ts
@@ -1,15 +1,15 @@
 import type { GenericModel } from '../../engine/models/Model'
+import Test from '../../engine/testing/Test.model'
 import Base from './Base'
-// import Test from '../../engine/testing/Test.model' // TODO: Migrate tests
 
 /** List of models connected to engine API/UI */
-const allModels /*: GenericModel[] */ = [
+const allModels = [
     Base,
     Base.getChildModel('demoArray'), // add child model to GUI/API
-] as const
+    // Test, // view model used for Tests
+]
 
-// @ts-ignore -- TODO: Create a generic 'Model' type that works, add 'Test' back in
-export default allModels as GenericModel[]
-
-/** Name of any Model connected to GUI/API */
+/** Names of Models connected to GUI/API */
 export type ModelTitle = typeof allModels[number]['title']
+
+export default allModels as GenericModel[]
