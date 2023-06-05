6c74cec669ab1f6a23ae29bf27d3dc5061438bba
Nick C <bathtaters@gmail.com>
2023-06-04 23:36:27 -0400

    remove 'module.exports' from msAgo

--- engine/libs/date.ts
+++ engine/libs/date.ts
@@ -4,10 +4,10 @@ const locale = require('date-fns/locale').enUS
 
 export { parseISO }
 
+export const now = Date.now
 export const
-  now = Date.now,
   isDate = (value: any): value is Date => isDateFNS(value),
-  msAgo = (date: number | Date) => differenceInMilliseconds(module.exports.now(), date),
+  msAgo = (date: number | Date) => differenceInMilliseconds(now(), date),
   formatLong =  (date?: number | Date | null) => date ? format(date, 'MM/dd/yy hh:mm:ssaaa', { locale }) : '-',
   formatShort = (date?: number | Date) => date ? format(date, 'MM/dd HH:mm', { locale }) : '',
   formatDateLong =  (date?: number | Date | null) => date ? format(date, 'MM/dd/yy', { locale }) : '-',
